b9368e6d0b5adda3d1fc69ff323191ca6fa24ddf
dzj0821 <dzj0821@126.com>
2019-10-23 20:28:01 +0800

    初始化

---
+++ src/main/java/cn/edu/chzu/chzuoj/ChzuojApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.chzu.chzuoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChzuojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChzuojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/chzu/chzuoj/ChzuojApplicationTest.java
@@ -0,0 +1,13 @@
+package cn.edu.chzu.chzuoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChzuojApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
